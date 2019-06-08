6ab0db4b10cd50e724b94b430d487dc41b138164
Daniel <nhanxtrand@gmail.com>
2019-06-08 05:55:15 +0000

    Refactor and commenting

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,124 @@
-import React, {Component} from 'react';
-import ChatBar from './ChatBar.jsx';
-import NavBar from './NavBar.jsx';
-import MessageList from './MessageList.jsx';
-
+import React, { Component } from "react";
+import ChatBar from "./ChatBar.jsx";
+import NavBar from "./NavBar.jsx";
+import MessageList from "./MessageList.jsx";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentUser: "Guest", 
+      currentUser: "Guest",
       messages: [],
       count: 0,
       color: "#2c3e50",
       imgURL: ""
-    }
-  };
+    };
+  }
 
   componentDidMount() {
+    /* SOCKET SERVER HANDLING
+    | ========================================================================== */
     const socket = new WebSocket("ws://localhost:3001/");
+    this.socket = socket;
 
     socket.onopen = () => {
-      console.log('Connected to server')
-    }
-    
-    socket.onmessage = (event) => {
+      console.log("Connected to server");
+    };
+
+    socket.onmessage = event => {
+      //Parse incoming data, add data to messages state array
       const data = JSON.parse(event.data);
-      const messages = this.state.messages.concat(data)
-      switch(data.type) {
+      const messages = this.state.messages.concat(data);
+
+      switch (data.type) {
         case "incomingMessage":
-          this.setState({currentUser: this.state.currentUser, messages})
+          this.setState({ 
+            currentUser: this.state.currentUser, 
+            messages 
+          });
         break;
+
         case "incomingNotification":
-          this.setState({messages})
+          this.setState({ messages });
         break;
+
         case "clientCount":
-          this.setState({count: data.count, messages});
+          this.setState({ 
+            count: data.count, 
+            messages 
+          });
         break;
+        //on connection assign username as guest and set each user a color
         case "onConnect":
-          this.setState({currentUser: data.currentUser, color: data.color})
+          this.setState({ 
+            currentUser: data.currentUser, 
+            color: data.color 
+          });
         break;
+        //on close decrement count
         case "onClose":
-          this.setState({currentUser: this.state.currentUser, count: data.count})
+          this.setState({
+            count: data.count
+          });
         break;
+        //handle image url and content message
         case "imageLink":
-          console.log(data.imgURL)
-          this.setState({imgURL: data.imgURL, messages})
+          this.setState({ imgURL: data.imgURL, messages });
         break;
+        //throw error if unknown case
         default:
-        // show an error in the console if the message type is unknown
         throw new Error("Unknown event type " + data.type);
       }
-    };  
-
-    this.socket = socket;
+    };
   }
-  
-  render() {
 
+  render() {
     return (
       <div>
-        < NavBar count={this.state.count} />
-        < MessageList messages={this.state.messages} imgURL={this.state.imgURL} />
-        < ChatBar currentUser={this.state.currentUser} usernameOnKeyPress={this.usernameKeyPressHandler} messageOnKeyPress={this.messageKeyPressHandler} />
+        <NavBar count={this.state.count} />
+        <MessageList
+          messages={this.state.messages}
+          imgURL={this.state.imgURL}
+        />
+        <ChatBar
+          currentUser={this.state.currentUser}
+          usernameOnKeyPress={this.usernameKeyPressHandler}
+          messageOnKeyPress={this.messageKeyPressHandler}
+        />
       </div>
     );
   }
 
+  /* EVENT HANDLERS
+    | ========================================================================== */
+  
+  //send data to server on "Enter"
+  messageKeyPressHandler = event => {
+    if (event.key === "Enter") {
+      const messageJSON = JSON.stringify({
+        id: "",
+        type: "incomingMessage",
+        username: this.state.currentUser,
+        content: event.target.value,
+        color: this.state.color
+      });
 
-
-
-  messageKeyPressHandler = (event) => {
-    if (event.key === 'Enter') {
-      const messageJSON = JSON.stringify({id: "", type: "incomingMessage", username: this.state.currentUser, content: event.target.value, color: this.state.color})
       this.socket.send(messageJSON);
       event.target.value = "";
-  }
-}
-
-  usernameKeyPressHandler = (event) => {
-
-    if (event.key === 'Enter') {
-      const notificationJSON = JSON.stringify({type: "incomingNotification", username: this.state.currentUser, content: `${this.state.currentUser} changed their name to ${event.target.value}`})
-      this.socket.send(notificationJSON);
-      this.setState({currentUser: event.target.value})
     }
-  }
-
+  };
+  //send data to display usernamer changes
+  usernameKeyPressHandler = event => {
+    if (event.key === "Enter") {
+      const notificationJSON = JSON.stringify({
+        type: "incomingNotification",
+        username: this.state.currentUser,
+        content: `${this.state.currentUser} changed their name to ${event.target.value}`
+      });
+      
+      this.socket.send(notificationJSON);
+      this.setState({ currentUser: event.target.value });
+    }
+  };
 }
 export default App;
