2c8285b6c69be121afcdb529ec94dee94ae9c024
Daniel <nhanxtrand@gmail.com>
2019-06-08 12:00:20 +0000

    Fixed issue with server crashing when sending images with text

--- chatty-server/server.js
+++ chatty-server/server.js
@@ -70,15 +70,16 @@ wss.on("connection", ws => {
 
     //if message contains an image link, broadcast message with type: "imageLink"
     if (isImg.test(parsedMessage.content)) {
+      //extract URL link and content
+      const imgURL = parsedMessage.content.match(isImg).slice(0, 1);
+      const message = parsedMessage.content.replace(isImg, "");
       wss.clients.forEach(function each(client) {
-        //extract URL link and content
-        const imgURL = parsedMessage.content.match(isImg).slice(0, 1);
-        const message = parsedMessage.content.replace(isImg, "");
-        parsedMessage.type = "imageLink";
-        parsedMessage.imgURL = imgURL;
-        parsedMessage.content = message;
-
+        
         if (client.readyState === WebSocket.OPEN) {
+          
+          parsedMessage.type = "imageLink";
+          parsedMessage.imgURL = imgURL;
+          parsedMessage.content = message;
           client.send(JSON.stringify({ parsedMessage }));
         }
       });
