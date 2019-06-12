6a321608aaf6cd4afb47a5060798e71262770d2d
Daniel <nhanxtrand@gmail.com>
2019-06-12 07:02:46 +0000

    Add helper function for wss.clients to broadcast to all users

--- chatty-server/server.js
+++ chatty-server/server.js
@@ -5,12 +5,9 @@ const WebSocket = require("ws");
 const SocketServer = require("ws").Server;
 const uuidv1 = require("uuid/v1");
 
-// Set the port to 3001
 const PORT = 3001;
 
-// Create a new express server
 const server = express()
-  // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static("public"))
   .listen(PORT, "0.0.0.0", "localhost", () =>
     console.log(`Listening on ${PORT}`)
@@ -18,7 +15,6 @@ const server = express()
 
 /* WS SERVER
 | =================================================================================================== */
-// Create the WebSockets server
 const wss = new SocketServer({ server });
 
 /* HELPER VARIABLES AND FUNCTIONS
@@ -29,13 +25,21 @@ const generateColor = () => {
   return colors[randomColor];
 };
 
+const broadCastToClients = messageObj => {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(messageObj));
+    }
+  });
+};
+
 /* ON CONNECTION HANDLING
 | ========================================================== */
 wss.on("connection", ws => {
   const color = generateColor();
   let connectionCount = wss.clients.size;
 
-  //on connection assign client a color
+  //assign client a color
   ws.send(
     JSON.stringify({
       color,
@@ -44,65 +48,40 @@ wss.on("connection", ws => {
     })
   );
 
-  //broadcast client count to all users
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(
-        JSON.stringify({
-          count: connectionCount,
-          type: "clientCount",
-          currentUser: `Guest${connectionCount}`,
-          content: `Guest${connectionCount} has entered the room.`
-        })
-      );
-    }
-  });
+  const message = {
+    count: connectionCount,
+    type: "clientCount",
+    currentUser: `Guest${connectionCount}`,
+    content: `Guest${connectionCount} has entered the room.`
+  };
 
-/* ON MESSAGE HANDLING
+  broadCastToClients(message);
+
+  /* ON MESSAGE HANDLING
 | ========================================================== */
-  //handle message when recieve a message on the front end
   ws.on("message", data => {
-    //check if image is a link
     const isImg = /https?:\/\/.*\.(?:png|jpg|gif)/i;
     const parsedMessage = JSON.parse(data);
     //add random Id
     parsedMessage.id = uuidv1();
 
-    //if message contains an image link, broadcast message with type: "imageLink"
     if (isImg.test(parsedMessage.content)) {
       //extract URL link and content
       const imgURL = parsedMessage.content.match(isImg).slice(0, 1);
       const message = parsedMessage.content.replace(isImg, "");
-      wss.clients.forEach(function each(client) {
-        
-        if (client.readyState === WebSocket.OPEN) {
-          parsedMessage.type = "incomingImageLink";
-          parsedMessage.imgURL = imgURL;
-          parsedMessage.content = message;
-          client.send(JSON.stringify({ parsedMessage }));
-        }
-      });
+      const messageObj = { type: "incomingImageLink", imgURL, message };
+
+      broadCastToClients(messageObj);
+    } else {
+      broadCastToClients(parsedMessage);
     }
-    //broadcast message
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(parsedMessage));
-      }
-    });
   });
 
-/* ON CLOSEHANDLING
+  /* ON CLOSEHANDLING
 | ========================================================== */
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on("close", () => {
     console.log("Client disconnected");
-    //broadcast decrement of client count
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(
-          JSON.stringify({ count: wss.clients.size, type: "onClose" })
-        );
-      }
-    });
+    const message = { count: wss.clients.size, type: "onClose" };
+    broadCastToClients(message);
   });
 });
